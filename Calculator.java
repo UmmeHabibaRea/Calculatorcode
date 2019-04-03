777837cc4cb714257b43cd00efbc1de4dfc2f954
Umme Habiba Rea <rea.ru18@gmail.com>
2019-04-03 10:28:46 +0600

    addition method added in Calculator class

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,9 @@
 public class Calculator{
 	public static void main(String[] args){
-		
+
+	}
+	public int addition(int firstvalue,int secondvalue){
+		int result = firstvalue + secondvalue;
+		return result;
 	}
 }
